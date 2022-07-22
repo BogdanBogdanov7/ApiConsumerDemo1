ce6f941a7f1e5d3e7c8361186935282de174b1b3
Bogdan <bogdan05@abv.bg>
2022-07-22 11:51:53 +0300

    initial commit

---
+++ ApiConsumerDemo1/SunInfo.xaml.cs
@@ -0,0 +1,33 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApp
+{
+    public partial class SunInfo : Window
+    {
+        public SunInfo()
+        {
+            InitializeComponent();
+        }
+        private async void loadSunInfo_Click(object sender, RoutedEventArgs e)
+        {
+            var sunInfo = await SunProcessor.LoadSunInformation();
+
+            sunriseText.Text = $"Sunrise is at {sunInfo.Sunrise.ToLocalTime().ToShortTimeString()}";
+            sunsetText.Text = $"Sunset is at {sunInfo.Sunset.ToLocalTime().ToShortTimeString()}";
+        }
+    }
+}
---
+++ DemoLibrary/SunResultModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class SunResultModel
+    {
+        public SunModel Results { get; set; }
+
+        public static implicit operator SunResultModel(ComicModel v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
